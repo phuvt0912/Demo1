4a142d9b18adf82ebd49bc5fdbcd45d55333cf19
Pham Sy Phu <phamsyphu0912@gmail.com>
2025-11-06 10:31:19 +0700

    Editting the MainActivity.kt file

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -5,16 +5,61 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+    private lateinit var statustxt: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate called")
+        statustxt = findViewById(R.id.txtStatus)
+        val goNextbtn: Button = findViewById(R.id.gonexttn)
+
+        goNextbtn.setOnClickListener {
+            val intent = Intent(this, SecondActivity:: class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy called")
     }
 }
--- app/src/main/java/com/example/demo/SecondActivity.kt
+++ app/src/main/java/com/example/demo/SecondActivity.kt
@@ -1,2 +1,52 @@
 package com.example.demo
 
+import android.app.PendingIntent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
+
+
+class SecondActivity: AppCompatActivity() {
+    private val TAG = "SecondActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_next)
+        Log.d(TAG, "onCreate called")
+
+        val Backbtn : Button = findViewById(R.id.gobackbtn)
+        Backbtn.setOnClickListener {
+            finish()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy called")
+    }
+}
